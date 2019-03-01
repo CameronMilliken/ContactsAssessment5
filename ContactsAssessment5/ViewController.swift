63498099f628d8b75c517e656c3e91b1938dc327
Cameron <cameron.milliken@gmail.com>
2019-03-01 09:09:01 -0700

    Initial commit

---
+++ ContactsAssessment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsAssessment5
+//
+//  Created by Cameron Milliken on 3/1/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
